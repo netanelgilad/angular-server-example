895c42d070328ac334d2f0a6bcd6a8429c75d530
Netanel Gilad <netanelgilad@gmail.com>
2015-05-04 14:43:09 +0300

    move module decleration

--- client/controllers/main.js
+++ client/controllers/main.js
@@ -1,24 +1,25 @@
 /**
  * Created by netanel on 27/02/15.
  */
-angular.module('todos').controller('MainController', function($scope, Todos, $meteorCollection, TodosManager, Data) {
-  $scope.todos = $meteorCollection(Todos.collection, false).subscribe('todos');
+angular.module('todos')
+  .controller('MainController', function($scope, Todos, $meteorCollection, TodosManager, Data) {
+    $scope.todos = $meteorCollection(Todos.collection, false).subscribe('todos');
 
-  $scope.newTask = {
-    text : '1'
-  };
+    $scope.newTask = {
+      text : '1'
+    };
 
-  $scope.addTodo = function () {
-    TodosManager.addTodo($scope.newTask);
-    $scope.newTask = {};
-  };
+    $scope.addTodo = function () {
+      TodosManager.addTodo($scope.newTask);
+      $scope.newTask = {};
+    };
 
-  $scope.doCoolThing = function() {
-    Data.getData().then(function(response) {
-      $scope.response = response;
-    }, function(error) {
-      $scope.response = 'Error:' + error;
-      console.log(error);
-    });
-  }
-});
+    $scope.doCoolThing = function() {
+      Data.getData().then(function(response) {
+        $scope.response = response;
+      }, function(error) {
+        $scope.response = 'Error:' + error;
+        console.log(error);
+      });
+    }
+  });
